1d8ac334c404cc639274c4204739d002951f2716
prateek <prateek.biz@gmail.com>
2018-10-22 22:53:20 +0530

    Added AuthorizationFailed exception

--- quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
@@ -3,6 +3,7 @@ package com.upgrad.quora.service.business;
 import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
@@ -74,9 +75,19 @@ public class AdminBusinessService {
         return userAuthEntity.getUser();
     }
 
-    public void deleteUser(final String uuid) throws UserNotFoundException {
+    public void deleteUser(final String uuid, final String authorizationToken) throws UserNotFoundException, AuthorizationFailedException {
 
         //UserEntity existingUser = userDao.getUserByUserName(userEntity.getUserName());
+
+        UserAuthEntity userAuthEntity = userDao.getUserAuthToken(authorizationToken);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "'User has not signed in");
+
+        }
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out");
+        }
+
         if (userDao.getUserById(uuid)==null) {
             throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
         }
