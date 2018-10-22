3ef43f9deaca662e9a82cac2f878f146352f5691
prateek <prateek.biz@gmail.com>
2018-10-22 22:05:58 +0530

    Added NamedQuery - "userById" and "deleteUserById". Added additional annotation @OnDelete to make uuid delete Cascade

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.upgrad.quora.service.entity;
 
 import org.apache.commons.lang3.builder.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -12,7 +14,9 @@ import java.io.Serializable;
 @NamedQueries(
         {
                 @NamedQuery(name = "userByUserName", query = "select u from UserEntity u where u.userName = :userName"),
-                @NamedQuery(name = "userByEmail", query = "select u from UserEntity u where u.email = :email")
+                @NamedQuery(name = "userByEmail", query = "select u from UserEntity u where u.email = :email"),
+                @NamedQuery(name = "userById", query = "select u from UserEntity u where u.uuid = :uid"),
+                @NamedQuery(name = "deleteUserById", query="delete from UserEntity u where u.uuid = :uid")
         }
 )
 public class UserEntity implements Serializable {
@@ -24,6 +28,7 @@ public class UserEntity implements Serializable {
 
     @Column(name = "uuid")
     @NotNull
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @Size(max = 200)
     private String uuid;
 
