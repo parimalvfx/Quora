21d5bf82cc1c2f9bc11ed386eeca2e40381ab681
prateek <prateek.biz@gmail.com>
2018-10-24 15:54:13 +0530

    Added getAllQuestionsByUser method to query all questions by userId from question table.

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -1,19 +1,25 @@
 package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class QuestionBusinessService {
 
     @Autowired
     private QuestionDao questionDao;
+    @Autowired
+    private UserDao userDao;
 
     @Transactional(propagation = Propagation.REQUIRED)
     public QuestionEntity createQuestion(QuestionEntity questionEntity, final String authorizationToken) throws AuthorizationFailedException {
@@ -27,5 +33,21 @@ public class QuestionBusinessService {
 
         questionEntity.setUser(userAuthEntity.getUser());
         return questionDao.createQuestion(questionEntity);
+    }
+
+    public List<QuestionEntity> getAllQuestionsByUser(final String uuid, final String authorizationToken) throws AuthorizationFailedException, UserNotFoundException{
+
+        UserAuthEntity userAuthEntity = questionDao.getUserAuthToken(authorizationToken);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "'User has not signed in");
+        }
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post a question");
+        }
+        if (userDao.getUserByUuid(uuid)==null) {
+            throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
+        }
+
+        return questionDao.getAllQuestionsByUser(uuid);
     }
 }
