c8c8f7a00c2e21b3e2b56d9c62cfa6e2152c6c10
Parimal Desai <parimalvfx@gmail.com>
2018-10-28 17:38:03 +0530

    Added CommonController class and userProfile endpoint

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -0,0 +1,35 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.UserDetailsResponse;
+import com.upgrad.quora.service.business.UserBusinessService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class CommonController {
+
+    @Autowired
+    private UserBusinessService userBusinessService;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}")
+    public ResponseEntity<UserDetailsResponse> userProfile(@PathVariable("userId") final String userId, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, UserNotFoundException {
+        UserEntity userEntity = userBusinessService.getUser(userId, authorization);
+
+        UserDetailsResponse userDetailsResponse = new UserDetailsResponse()
+                .firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName())
+                .userName(userEntity.getUserName())
+                .emailAddress(userEntity.getEmail())
+                .country(userEntity.getCountry())
+                .aboutMe(userEntity.getAboutMe())
+                .dob(userEntity.getDob())
+                .contactNumber(userEntity.getContactNumber());
+        return new ResponseEntity<UserDetailsResponse>(userDetailsResponse, HttpStatus.OK);
+    }
+}
