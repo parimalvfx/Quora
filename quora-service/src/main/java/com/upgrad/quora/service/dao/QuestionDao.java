eacecdcfd727277d97e4926d0e5bcadad5291a60
Parimal Desai <parimalvfx@gmail.com>
2018-10-28 23:59:40 +0530

    Corrected getQuestionByUuid

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -38,7 +38,7 @@ public class QuestionDao {
 
     public QuestionEntity getQuestionByUuid(final String uuid) {
         try {
-            return entityManager.createNamedQuery("questionEntityByUuid", QuestionEntity.class).setParameter("uuid",uuid).getSingleResult();
+            return entityManager.createNamedQuery("questionByUserId", QuestionEntity.class).setParameter("uuid",uuid).getSingleResult();
         } catch (NoResultException nre) {
             return null;
         }
